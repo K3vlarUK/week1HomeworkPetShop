0353681fc776f78328fc7a89ea9f22b74a5291e5
Kevin McDermott <kmcdermott31@gmail.com>
2019-08-30 13:56:32 +0100

    Most of homework complete

---
+++ wk1hw/start_point/pet_shop.rb
@@ -0,0 +1,78 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  pet_shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, num_pets_sold)
+  pet_shop[:admin][:pets_sold] += num_pets_sold
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length()
+end
+
+def pets_by_breed(pet_shop, pet_breed)
+  pets = []
+  for pet in pet_shop[:pets]
+    if pet[:breed] == pet_breed
+      pets.push(pet)
+    end
+  end
+  return pets
+end
+
+def find_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      name_of_pet = pet
+    end
+  end
+  return name_of_pet
+end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      pet_shop[:pets].delete(pet)
+    end
+  end
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
+end
+
+def customer_cash(customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
+end
+
+def customer_pet_count(customer)
+  customer[:pets].length()
+end
+
+def add_pet_to_customer(customer, pet)
+  customer[:pets].push(pet)
+end
+
+def customer_can_afford_pet(customer, pet)
+  if pet[:price] > customer[:cash]
+    return false
+  else
+    return true
+  end
+end
